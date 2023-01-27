b98a17b3aa4d6868ac25e8ba65c8a21d2320c585
Peter Choi <jy.peter.c@gmail.com>
2023-01-26 22:55:03 -0600

    set playerstatus and work on functions

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+/* eslint-disable */
 let gameBoard = Array(9).fill(null);
-
-let playerStatus = playerX;
 
 let playerX = 'X';
 let playerO = 'O';
 
-document.getElementsByClassName("cell")
+let playerStatus = playerX;
+
+
+
+for (let i = 1; i < 10; i++){
+    let cell = document.getElementById(i.toString());
+    cell.addEventListener("click", function(){
+        cell.innerHTML = playerStatus;
+        if(playerStatus == playerX){
+            playerStatus = playerO;
+        }else playerStatus = playerX;
+    })
+}
+console.log(playerStatus);
