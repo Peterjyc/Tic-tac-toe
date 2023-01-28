f6c06d2fb1ffa2f86e11219b0c7884a969e92af8
Peter Choi <jy.peter.c@gmail.com>
2023-01-27 23:23:37 -0600

    js work

--- script.js
+++ script.js
@@ -11,10 +11,19 @@ let playerStatus = playerX;
 for (let i = 1; i < 10; i++){
     let cell = document.getElementById(i.toString());
     cell.addEventListener("click", function(){
+
+
         cell.innerHTML = playerStatus;
         if(playerStatus == playerX){
+            gameBoard[i] = playerX;
             playerStatus = playerO;
-        }else playerStatus = playerX;
+        }else {
+            gameBoard[i] = playerO;
+            playerStatus = playerX;
+        }
+        console.log(playerStatus);
+        console.log(gameBoard);
     })
 }
 console.log(playerStatus);
+console.log(gameBoard);
